35c80716fc3ec92172a655cedc2f0624e3212620
FilipeMts <jfgmatos@outlook.com>
2020-05-03 08:09:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# freeCodeCamp Front End Libraries Projects: Drum Machine
+
+In late-2018 / early-2019 I went through freeCodeCamp’s Frontend curriculum while working full-time in a non-programming job. 
+
+This was my take on the Drum Machine project: https://filipemts.github.io/FCC-Drum-Machine
+
+Tech used:
+  - React
+  - SASS
---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { useEffect } from 'react';
+import './App.scss';
+
+const App = () => {  
+  const drumBeats = [{
+      keyCode: 81,
+      letter: 'Q',
+      title: 'bass',
+      url: 'https://www.fesliyanstudios.com/play-mp3/6660'
+    }, 
+    {
+      keyCode: 87,
+      letter: 'W',
+      title: 'tom drum',
+      url: 'https://www.fesliyanstudios.com/play-mp3/6694'
+    },
+    {
+      keyCode: 69,
+      letter: 'E',
+      title: 'cymbal',
+      url: 'https://www.fesliyanstudios.com/play-mp3/6732'
+    },
+    {
+      keyCode: 65,
+      letter: 'A',
+      title: 'clap',
+      url: 'https://www.myinstants.com/media/sounds/behemoth-clap-15.mp3'
+    },
+    {
+      keyCode: 83,
+      letter: 'S',
+      title: 'sticks',
+      url: 'https://www.fesliyanstudios.com/play-mp3/6677'
+    },
+    {
+      keyCode: 68,
+      letter: 'D',
+      title: 'beat',
+      url: 'https://www.myinstants.com/media/sounds/snare.mp3'
+    },
+    {
+      keyCode: 90,
+      letter: 'Z',
+      title: "hi hat",
+      url: 'https://www.fesliyanstudios.com/play-mp3/6706'
+    },
+    {
+      keyCode: 88,
+      letter: 'X',
+      title: 'snare',
+      url: 'https://www.fesliyanstudios.com/play-mp3/6768'
+    }, 
+    {
+      keyCode: 67,
+      letter: 'C',
+      title: 'ba dum tisss',
+      url: 'https://www.myinstants.com/media/sounds/tun-tun-tisss.mp3'
+    }
+  ];
+
+  const playBeat = el => {
+    const audioElem = document.getElementById(el.letter);
+    const beatTitle = document.querySelector('.what-sound');
+    beatTitle.textContent = el.title
+    audioElem.src = el.url;
+    audioElem.play();
+    audioElem.currentTime = 0;
+  };
+
+  useEffect(() => {
+    console.log('use effect');
+
+    const handleKeyPress = e => {
+      let beat = drumBeats.filter(obj => obj.keyCode === e.keyCode)[0];
+      if (beat !== undefined) playBeat(beat);
+    };
+
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [drumBeats]);
+
+  return (
+    <div className='App'>
+      <div className='background'></div>
+      <div id='drum-machine'>
+        <div id='display'>
+          <div className="drum-pad-wrapper">
+          {drumBeats.map(el => 
+            <div id={el.title} className='drum-pad' key={el.keyCode} onClick={() => playBeat(el)}>
+              <p>{el.letter}</p>
+              <audio className='clip' src={el.url} id={el.letter} ></audio>
+            </div>
+          )}          
+          
+          </div>
+          <p className='what-sound'>Hit the drums or press the keys</p>          
+        </div>      
+        <div className="code">
+          <a href="/" target="_blank" rel="noopener noreferrer">code is available here</a>
+        </div> 
+        </div>
+    </div>
+  );
+};
+
+export default App;
